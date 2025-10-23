352b17d0739143aad333979e51e0e6fdc93d08b6
akmaalsyh <taufansyah50@gmail.com>
2025-10-23 16:01:14 +0700

    memperbaiki code

--- 20230140236-node-server/server.js
+++ 20230140236-node-server/server.js
@@ -4,29 +4,25 @@ const app = express();
 const PORT = 3001;
 const morgan = require("morgan");
 
-const bookRoutes = require("./routes/books");
+// Impor router
 const presensiRoutes = require("./routes/presensi");
 const reportRoutes = require("./routes/reports");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
 });
-
-app.use("/api/books", bookRoutes);
-
 app.get("/", (req, res) => {
   res.send("Home Page for API");
 });
-
 const ruteBuku = require("./routes/books");
 app.use("/api/books", ruteBuku);
 app.use("/api/presensi", presensiRoutes);
 app.use("/api/reports", reportRoutes);
-
 app.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`);
 });
